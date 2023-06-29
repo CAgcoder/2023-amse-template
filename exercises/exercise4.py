356caf60e88c0fb3f6768cd165b5c510ac93bdf1
Tinggao Cui <tginfo.cui@fau.de>
2023-06-29 21:44:53 +0200

    4th try

--- exercises/exercise4.py
+++ exercises/exercise4.py
@@ -3,71 +3,81 @@ import zipfile
 import pandas as pd
 import sqlite3
 
-# Step 1: Download and unzip the data
-zip_file_url = "https://www.mowesta.com/data/measure/mowesta-dataset-20221107.zip"
-data_file_name = "data.csv"
-zip_file_name = "exercises/mowesta-dataset.zip"
 
-urllib.request.urlretrieve(zip_file_url, zip_file_name)
+def zip():
+    zip_file_url = "https://www.mowesta.com/data/measure/mowesta-dataset-20221107.zip"
+    data_file_name = "data.csv"
+    zip_file_name = "exercises/mowesta-dataset.zip"
 
-with zipfile.ZipFile(zip_file_name) as zip_ref:
-    zip_ref.extract(data_file_name, path="exercises")
+    urllib.request.urlretrieve(zip_file_url, zip_file_name)
 
-
-df = pd.read_csv(
-    "exercises/data.csv",
-    usecols=range(15),
-    encoding="utf-8",
-    sep=";",
-)
-df = df.reset_index()
-rename_columns = {
-    "level_0": "Geraet",
-    "level_1": "Hersteller",
-    "level_2": "Model",
-    "level_3": "Monat",
-    "level_4": "Temperatur",
-    "level_5": "Latitude (WGS84)",
-    "level_6": "Longitude (WGS84)",
-    "level_7": "Verschleierung (m)",
-    "level_8": "Aufenthaltsdauer im Freien (ms)",
-    "level_9": "Batterietemperatur",
-    "level_10": "Geraet aktiv",
-}
-df.rename(
-    columns=rename_columns,
-    inplace=True,
-)
-df = df.iloc[:, :11]
-df = df.drop(
-    [
-        "Verschleierung (m)",
-        "Aufenthaltsdauer im Freien (ms)",
-        "Longitude (WGS84)",
-        "Latitude (WGS84)",
-    ],
-    axis=1,
-)
+    with zipfile.ZipFile(zip_file_name) as zip_ref:
+        zip_ref.extract(data_file_name, path="exercises")
 
 
-df["Temperatur"] = df["Temperatur"].str.replace(",", ".")
-df["Temperatur"] = df["Temperatur"].astype(float)
-df["Temperatur"] = (df["Temperatur"] * 9 / 5) + 32
-df["Temperatur"] = df["Temperatur"].round(2)
+def csv():
+    df = pd.read_csv(
+        "exercises/data.csv",
+        usecols=range(15),
+        encoding="utf-8",
+        sep=";",
+    )
+    df = df.reset_index()
+    rename_columns = {
+        "level_0": "Geraet",
+        "level_1": "Hersteller",
+        "level_2": "Model",
+        "level_3": "Monat",
+        "level_4": "Temperatur",
+        "level_5": "Latitude (WGS84)",
+        "level_6": "Longitude (WGS84)",
+        "level_7": "Verschleierung (m)",
+        "level_8": "Aufenthaltsdauer im Freien (ms)",
+        "level_9": "Batterietemperatur",
+        "level_10": "Geraet aktiv",
+    }
+    df.rename(
+        columns=rename_columns,
+        inplace=True,
+    )
+    df = df.iloc[:, :11]
+    df = df.drop(
+        [
+            "Verschleierung (m)",
+            "Aufenthaltsdauer im Freien (ms)",
+            "Longitude (WGS84)",
+            "Latitude (WGS84)",
+        ],
+        axis=1,
+    )
 
-df["Batterietemperatur"] = df["Batterietemperatur"].str.replace(",", ".")
-df["Batterietemperatur"] = df["Batterietemperatur"].astype(float)
-df["Batterietemperatur"] = (df["Batterietemperatur"] * 9 / 5) + 32
-df["Batterietemperatur"] = df["Batterietemperatur"].round(2)
+    df["Temperatur"] = df["Temperatur"].str.replace(",", ".")
+    df["Temperatur"] = df["Temperatur"].astype(float)
+    df["Temperatur"] = (df["Temperatur"] * 9 / 5) + 32
+    df["Temperatur"] = df["Temperatur"].round(2)
+
+    df["Batterietemperatur"] = df["Batterietemperatur"].str.replace(",", ".")
+    df["Batterietemperatur"] = df["Batterietemperatur"].astype(float)
+    df["Batterietemperatur"] = (df["Batterietemperatur"] * 9 / 5) + 32
+    df["Batterietemperatur"] = df["Batterietemperatur"].round(2)
+
+    df = df[df["Geraet"] > 0]
+    return df
 
 
-df = df[df["Geraet"] > 0]
-df = df[df["Geraet"] > 0]
+def store_data(df):
+    database_name = "exercises/temperatures.sqlite"
+    table_name = "temperatures"
+    conn = sqlite3.connect(database_name)
+    df.to_sql(table_name, conn, if_exists="replace", index=False)
+    conn.close()
 
-database_name = "exercises/temperatures.sqlite"
-table_name = "temperatures"
 
-conn = sqlite3.connect(database_name)
-df.to_sql(table_name, conn, if_exists="replace", index=False)
+def main():
+    zip()
+    df = csv()
+    store_data(df)
 
-conn.close()
+
+if __name__ == "__main__":
+    main()
